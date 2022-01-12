fe1c368fac55a5aa69369b40b4eeb6644d1c6428
Akif <dereakif@gmail.com>
2022-01-12 18:29:24 +0300

    replace: isInvalid with error

--- src/components/NoResult/index.tsx
+++ src/components/NoResult/index.tsx
@@ -1,17 +1,17 @@
 import './styles.scss';
 
 interface Props {
-  isInvalid: boolean;
+  error: string | null;
 }
 
-const NoResult = ({ isInvalid }: Props) => {
+const NoResult = ({ error }: Props) => {
   return (
     <div className='no-result__container'>
-      <h1 className={isInvalid ? 'no-result__container--invalid-red' : ''}>
-        {isInvalid ? 'No city is selected!' : 'City doesn’t exist!'}
+      <h1 className={error ? 'no-result__container--invalid-red' : ''}>
+        {error ? 'No city is selected!' : 'City doesn’t exist!'}
       </h1>
       <p>
-        {isInvalid
+        {error
           ? 'Type a valid city name to get weekly forecast data'
           : 'Type any city name to get weekly forecast data'}
       </p>
