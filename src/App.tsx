99a37c808c9d4286d532de71073fe938f35050e4
Akif <dereakif@gmail.com>
2022-01-12 03:53:02 +0300

    done: SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.scss';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <SearchBar />
     </div>
   );
 }
